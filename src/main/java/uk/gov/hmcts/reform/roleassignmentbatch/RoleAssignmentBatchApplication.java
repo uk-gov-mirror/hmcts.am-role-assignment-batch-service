49f92a011293645105cdd4eb4a5b624849f802d2
Nitish Singla <nitish.singla@hmcts.net>
2020-06-16 12:39:19 +0100

    Initial checkin for the batch service.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
@@ -14,7 +14,7 @@ public class RoleAssignmentBatchApplication {
     public static void main(String[] args) throws Exception {
         ApplicationContext context = SpringApplication.run(RoleAssignmentBatchApplication.class, args);
         //Sleep added to allow app-insights to flush the logs
-        Thread.sleep(5000);
+        Thread.sleep(6000);
         int exitCode = SpringApplication.exit(context);
         log.info("RoleAssignmentBatchApplication Application exiting with exit code " + exitCode);
         System.exit(exitCode);
