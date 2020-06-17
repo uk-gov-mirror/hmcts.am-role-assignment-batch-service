7039df56a97b7d88fce4b8ef11156879a60d8762
Nitish Singla <nitish.singla@hmcts.net>
2020-06-17 12:28:19 +0100

    Initial checkin for the batch service.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
@@ -3,9 +3,10 @@ package uk.gov.hmcts.reform.roleassignmentbatch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Slf4j
 @SuppressWarnings("HideUtilityClassConstructor")
 public class RoleAssignmentBatchApplication {
