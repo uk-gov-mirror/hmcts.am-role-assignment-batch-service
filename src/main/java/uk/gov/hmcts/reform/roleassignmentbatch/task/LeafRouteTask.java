6fe633bada4786a9f1dd36b2e4672bdb6e57fa5a
Nitish Singla <nitish.singla@hmcts.net>
2020-06-17 10:34:46 +0100

    Initial checkin for the batch service.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/task/LeafRouteTask.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/task/LeafRouteTask.java
@@ -15,6 +15,18 @@ public class LeafRouteTask implements Tasklet {
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
         log.info("::LeafRouteTask starts::");
 
+        //steps: 1. Create a datasource in application.yaml file.
+        // Please refer to Role-assignment-service project's application.yaml for that
+        // https://github.com/hmcts/am-role-assignment-service
+        //(UPDATE: Datasource has been configured. Pleae verify)
+        //2. While creating the datasource, we will need to setup the environment variables as in https://github.com/hmcts/am-role-assignment-service
+        //Same variables can be reused.
+
+        //3. Once datasource is ready, write a query using Spring JDBC to fetch the number of records from Role_Assignment table
+        //Reference: https://spring.io/guides/gs/relational-data-access/
+
+        //test the changes in local docker
+
         log.info("::LeafRouteTask completes with {}::", "success");
         return RepeatStatus.FINISHED;
     }
