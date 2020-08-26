13edc8b67305b338790d94bcb255a0f1beda4b5c
Nitish Singla <nitish.singla@hmcts.net>
2020-08-26 16:56:34 +0100

    AM-972: Added initial Integration test.

--- src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentBatchJobIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/roleassignment/RoleAssignmentBatchJobIntegrationTest.java
@@ -26,8 +26,10 @@ public class RoleAssignmentBatchJobIntegrationTest extends BaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(RoleAssignmentBatchJobIntegrationTest.class);
 
-    DeleteExpiredRecords sut = new DeleteExpiredRecords();
-    private static final String COUNT_RECORDS = "SELECT count(*) as n FROM role_assignment";
+    DeleteExpiredRecords sut;
+    private static final String COUNT_RECORDS_FROM_LIVE_TABLE = "SELECT count(*) as n FROM role_assignment";
+    private static final String COUNT_EXPIRED_RECORDS_FROM_HISTORY_TABLE =
+        "SELECT count(*) as n FROM role_assignment_history where STATUS='EXPIRED'";
 
     @Autowired
     private DataSource ds;
@@ -37,9 +39,8 @@ public class RoleAssignmentBatchJobIntegrationTest extends BaseTest {
     @Before
     public void setUp() {
         template = new JdbcTemplate(ds);
-        sut.setJdbcTemplate(template);
+        sut = new DeleteExpiredRecords(template, 2);
     }
-
 
     @Test
     @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
@@ -48,7 +49,7 @@ public class RoleAssignmentBatchJobIntegrationTest extends BaseTest {
                     "classpath:sql/insert_role_assignment_history.sql",
                     "classpath:sql/insert_role_assignment.sql"})
     public void shouldGetRecordCountFromLiveTable() {
-        final Integer count = template.queryForObject(COUNT_RECORDS, Integer.class);
+        final Integer count = template.queryForObject(COUNT_RECORDS_FROM_LIVE_TABLE, Integer.class);
         logger.info(" Total number of records fetched from role assignment Live table...{}", count);
         assertNotNull(count);
         assertEquals(
@@ -62,13 +63,30 @@ public class RoleAssignmentBatchJobIntegrationTest extends BaseTest {
                     "classpath:sql/insert_role_assignment_history.sql",
                     "classpath:sql/insert_role_assignment.sql"})
     public void shouldDeleteRecordsFromLiveTable() {
-        Integer count = template.queryForObject(COUNT_RECORDS, Integer.class);
+        Integer count = template.queryForObject(COUNT_RECORDS_FROM_LIVE_TABLE, Integer.class);
         logger.info(" Total number of records fetched from role assignment Live table...{}", count);
         logger.info(" Deleting the records from Live table.");
-        sut.execute(null,null);
-        count = template.queryForObject(COUNT_RECORDS, Integer.class);
+        sut.execute(null, null);
+        count = template.queryForObject(COUNT_RECORDS_FROM_LIVE_TABLE, Integer.class);
         logger.info(" Total number of records fetched from role assignment Live table...{}", count);
-        Assert.assertEquals("The live records were not deleted", count, Integer.valueOf(0));
+        Assert.assertEquals("The live records were not deleted", Integer.valueOf(0), count);
+    }
+
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+         scripts = {"classpath:sql/role_assignment_clean_up.sql",
+                    "classpath:sql/insert_role_assignment_request.sql",
+                    "classpath:sql/insert_role_assignment_history.sql",
+                    "classpath:sql/insert_role_assignment.sql"})
+    public void shouldInsertRecordsInHistoryTable() {
+        Integer count = template.queryForObject(COUNT_EXPIRED_RECORDS_FROM_HISTORY_TABLE, Integer.class);
+        logger.info(" Total number of expired records fetched from History table...{}", count);
+        Assert.assertEquals("The live records were not deleted", Integer.valueOf(0), count);
+        logger.info(" Deleting the records from Live table. Insert the records in History table.");
+        sut.execute(null, null);
+        count = template.queryForObject(COUNT_EXPIRED_RECORDS_FROM_HISTORY_TABLE, Integer.class);
+        logger.info(" Total number of Expired records fetched from History table...{}", count);
+        Assert.assertEquals("The EXPIRED records were not inserted", Integer.valueOf(5), count);
     }
 
 }
--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/task/DeleteExpiredRecords.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/task/DeleteExpiredRecords.java
@@ -7,6 +7,7 @@ import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
@@ -16,19 +17,19 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ParameterizedPreparedStatementSetter;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@AllArgsConstructor
 public class DeleteExpiredRecords implements Tasklet {
 
     private static final Logger log = LoggerFactory.getLogger(DeleteExpiredRecords.class);
 
     @Autowired
-    private
-    JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;
 
     @Value("${batch-size}")
     int batchSize;
@@ -79,39 +80,39 @@ public class DeleteExpiredRecords implements Tasklet {
         return rows;
     }
 
-    public int[] insertIntoRoleAssignmentHistoryTable(List<RoleAssignmentHistory> rah) {
-        return getJdbcTemplate().batchUpdate("INSERT INTO role_assignment_history "
-                                             + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                                             new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        ps.setObject(1, rah.get(i).getId());
-                        ps.setObject(2, rah.get(i).getRequestId());
-                        ps.setString(3, rah.get(i).getActorIDType());
-                        ps.setObject(4, rah.get(i).getActorId());
-                        ps.setString(5, rah.get(i).getRoleType());
-                        ps.setString(6, rah.get(i).getRoleName());
-                        ps.setString(7, rah.get(i).getClassification());
-                        ps.setString(8, rah.get(i).getGrantType());
-                        ps.setString(9, rah.get(i).getRoleCategory());
-                        ps.setBoolean(10, rah.get(i).isReadOnly());
-                        ps.setTimestamp(11, rah.get(i).getBeginTime());
-                        ps.setTimestamp(12, rah.get(i).getEndTime());
-                        ps.setString(13, rah.get(i).getStatus());
-                        ps.setString(14, rah.get(i).getReference());
-                        ps.setString(15, rah.get(i).getProcess());
-                        ps.setString(16, rah.get(i).getAttributes());
-                        ps.setString(17, rah.get(i).getNotes());
-                        ps.setString(18, rah.get(i).getLog());
-                        ps.setInt(19, rah.get(i).getStatusSequence());
-                        ps.setTimestamp(20, rah.get(i).getCreated());
-                    }
+    public int[][] insertIntoRoleAssignmentHistoryTable(List<RoleAssignmentHistory> rah) {
+        return getJdbcTemplate().batchUpdate(
+            "INSERT INTO role_assignment_history "
+            + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+            rah,
+            batchSize,
+            new ParameterizedPreparedStatementSetter<RoleAssignmentHistory>() {
+                @Override
+                public void setValues(PreparedStatement ps, RoleAssignmentHistory roleAssignmentHistory)
+                    throws SQLException {
+                    ps.setObject(1, roleAssignmentHistory.getId());
+                    ps.setObject(2, roleAssignmentHistory.getRequestId());
+                    ps.setString(3, roleAssignmentHistory.getActorIDType());
+                    ps.setObject(4, roleAssignmentHistory.getActorId());
+                    ps.setString(5, roleAssignmentHistory.getRoleType());
+                    ps.setString(6, roleAssignmentHistory.getRoleName());
+                    ps.setString(7, roleAssignmentHistory.getClassification());
+                    ps.setString(8, roleAssignmentHistory.getGrantType());
+                    ps.setString(9, roleAssignmentHistory.getRoleCategory());
+                    ps.setBoolean(10, roleAssignmentHistory.isReadOnly());
+                    ps.setTimestamp(11, roleAssignmentHistory.getBeginTime());
+                    ps.setTimestamp(12, roleAssignmentHistory.getEndTime());
+                    ps.setString(13, roleAssignmentHistory.getStatus());
+                    ps.setString(14, roleAssignmentHistory.getReference());
+                    ps.setString(15, roleAssignmentHistory.getProcess());
+                    ps.setString(16, roleAssignmentHistory.getAttributes());
+                    ps.setString(17, roleAssignmentHistory.getNotes());
+                    ps.setString(18, roleAssignmentHistory.getLog());
+                    ps.setInt(19, roleAssignmentHistory.getStatusSequence());
+                    ps.setTimestamp(20, roleAssignmentHistory.getCreated());
+                }
 
-                    @Override
-                    public int getBatchSize() {
-                        return rah.size() > batchSize ? batchSize : rah.size();
-                    }
-                });
+            });
     }
 
 
@@ -157,9 +158,5 @@ public class DeleteExpiredRecords implements Tasklet {
 
     public JdbcTemplate getJdbcTemplate() {
         return jdbcTemplate;
-    }
-
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
     }
 }
