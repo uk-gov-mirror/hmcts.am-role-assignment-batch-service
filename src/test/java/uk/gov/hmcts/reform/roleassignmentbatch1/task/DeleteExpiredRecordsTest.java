da50406fc1a5a74fd4c715c1ee29fd8cfa04ff8b
ConorH994 <conor.halpin@hmcts.net>
2020-09-24 09:34:12 +0100

    exception test

--- src/test/java/uk/gov/hmcts/reform/roleassignmentbatch1/task/DeleteExpiredRecordsTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignmentbatch1/task/DeleteExpiredRecordsTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.roleassignmentbatch1.task;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -14,9 +15,11 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -24,6 +27,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import uk.gov.hmcts.reform.roleassignmentbatch1.helper.TestDataBuilder;
 
+@RunWith(MockitoJUnitRunner.class)
 class DeleteExpiredRecordsTest {
 
     @Mock
@@ -38,7 +42,7 @@ class DeleteExpiredRecordsTest {
     private DeleteExpiredRecords sut = new DeleteExpiredRecords(jdbcTemplate, 5);
 
     @Before
-    public void setUp() {
+    void setUp() {
         MockitoAnnotations.initMocks(this);
     }
 
@@ -56,13 +60,27 @@ class DeleteExpiredRecordsTest {
     }
 
     @Test
+    void executeThrowsException() throws IOException {
+        when(jdbcTemplate.queryForObject(anyString(), eq(Integer.class)))
+                .thenThrow(NullPointerException.class);
+
+        List<RoleAssignmentHistory> list = new ArrayList<>();
+        list.add(TestDataBuilder.buildRoleAssignmentHistory());
+
+        when(jdbcTemplate.query(anyString(), ArgumentMatchers.<ResultSetExtractor<Object>>any())).thenReturn(list);
+
+        Assertions.assertThrows(NullPointerException.class, () ->
+                sut.execute(stepContribution, chunkContext)
+        );
+    }
+
+    @Test
     void deleteRoleAssignmentRecords() throws IOException {
 
         List<RoleAssignmentHistory> list = new ArrayList<>();
         list.add(TestDataBuilder.buildRoleAssignmentHistory());
 
-        when(jdbcTemplate.update(any(), any(), any()))
-            .thenReturn(1);
+        when(jdbcTemplate.update(any(), any(), any())).thenReturn(1);
 
         Assertions.assertEquals(1, sut.deleteRoleAssignmentRecords(list));
     }
@@ -81,10 +99,8 @@ class DeleteExpiredRecordsTest {
 
     @Test
     void getLiveRecordsFromHistoryTable() throws IOException {
-
         List<RoleAssignmentHistory> list = new ArrayList<>();
         list.add(TestDataBuilder.buildRoleAssignmentHistory());
-
         when(jdbcTemplate.query(anyString(), ArgumentMatchers.<ResultSetExtractor<Object>>any())).thenReturn(list);
         Assertions.assertEquals(list, sut.getLiveRecordsFromHistoryTable());
     }
