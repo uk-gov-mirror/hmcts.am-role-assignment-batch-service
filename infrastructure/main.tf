40791c1ad648fb65d1ed66591238068241a2784a
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2020-10-01 11:55:18 +0100

    Remove commented TF code (#17)
    * Remove unnecessary TF code
    
    * Remove commented code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,26 +1,3 @@
 locals {
   app_full_name = join("-", [var.product, var.component])
-  
-  # local_env = (var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env
-
-  # // Vault name
-  # previewVaultName = "${var.raw_product}-aat"
-  # nonPreviewVaultName = "${var.raw_product}-${var.env}"
-  # vaultName = (var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName
-
-  # // Shared Resource Group
-  # previewResourceGroup = "${var.raw_product}-shared-infrastructure-aat"
-  # nonPreviewResourceGroup = "${var.raw_product}-shared-infrastructure-${var.env}"
-  # sharedResourceGroup = (var.env == "preview" || var.env == "spreview") ? local.previewResourceGroup : local.nonPreviewResourceGroup
-
 }
-
-# data "azurerm_key_vault" "am_key_vault" {
-#   name = local.vaultName
-#   resource_group_name = local.sharedResourceGroup
-# }
-
-# data "azurerm_key_vault" "s2s_vault" {
-#   name = "s2s-${local.local_env}"
-#   resource_group_name = "rpe-service-auth-provider-${local.local_env}"
-# }
