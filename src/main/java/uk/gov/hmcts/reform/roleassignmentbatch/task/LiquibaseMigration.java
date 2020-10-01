f7fa7f6add40a9b993e1d3879abc49799d6e84c6
Nitish Singla <nitish.singla@hmcts.net>
2020-10-01 10:03:25 +0100

    Adding liquibase changes to project to support PR build.

---
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/task/LiquibaseMigration.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.roleassignmentbatch.task;
+
+import javax.sql.DataSource;
+
+import liquibase.Contexts;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LiquibaseMigration implements Tasklet {
+
+    @Autowired
+    DataSource dataSource;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LiquibaseMigration.class);
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
+            new JdbcConnection(dataSource.getConnection()));
+        try (Liquibase liquibase = new Liquibase("db/changelog/db.changelog-master.xml",
+                                                 new ClassLoaderResourceAccessor(), database
+        )) {
+            liquibase.update(new Contexts());
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage());
+        }
+        return null;
+    }
+}
