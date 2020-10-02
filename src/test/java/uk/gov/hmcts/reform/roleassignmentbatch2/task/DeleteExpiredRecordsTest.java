fd7420e14eb1129d245e5c54677d915c920df0f0
Nitin <nitin.sahu@hmcts.net>
2020-10-02 10:31:08 +0100

    Added pitest dependency and more junit tests

--- src/test/java/uk/gov/hmcts/reform/roleassignmentbatch2/task/DeleteExpiredRecordsTest.java
+++ src/test/java/uk/gov/hmcts/reform/roleassignmentbatch2/task/DeleteExpiredRecordsTest.java
@@ -9,8 +9,11 @@ import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
@@ -25,6 +28,12 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import uk.gov.hmcts.reform.roleassignmentbatch2.domain.model.enums.Status;
+import uk.gov.hmcts.reform.roleassignmentbatch2.domain.model.enums.ActorIdType;
+import uk.gov.hmcts.reform.roleassignmentbatch2.domain.model.enums.RoleType;
+import uk.gov.hmcts.reform.roleassignmentbatch2.domain.model.enums.Classification;
+import uk.gov.hmcts.reform.roleassignmentbatch2.domain.model.enums.GrantType;
+import uk.gov.hmcts.reform.roleassignmentbatch2.domain.model.enums.RoleCategory;
 import uk.gov.hmcts.reform.roleassignmentbatch2.helper.TestDataBuilder;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -45,7 +54,7 @@ class DeleteExpiredRecordsTest {
     private DeleteExpiredRecords sut = new DeleteExpiredRecords(jdbcTemplate, 5);
 
     @Before
-    void setUp() {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
     }
 
@@ -108,9 +117,12 @@ class DeleteExpiredRecordsTest {
         Assertions.assertEquals(list, sut.getLiveRecordsFromHistoryTable());
     }
 
-
     @Test
-    public void getLiveRecordsFromHistoryTableWithValidValues() {
+    void getLiveRecordsFromHistoryTableWithValidValues() {
+        LocalDateTime timeStamp = LocalDateTime.now();
+        Timestamp beginDate = Timestamp.valueOf(timeStamp.plusDays(1));
+        Timestamp endDate = Timestamp.valueOf(timeStamp.plusMonths(1));
+        Timestamp created = Timestamp.valueOf(timeStamp);
         when(jdbcTemplate.query(
                 ArgumentMatchers.anyString(), ArgumentMatchers.<ResultSetExtractor<Object>>any()))
                 .thenAnswer((invocation) -> {
@@ -118,24 +130,51 @@ class DeleteExpiredRecordsTest {
                     final ResultSetExtractor<List<RoleAssignmentHistory>> resultSetExtractor =
                             invocation.getArgument(1);
                     when(rs.next()).thenReturn(true, false);
-                    when(rs.getObject(ArgumentMatchers.eq("id"))).thenReturn(java.util.UUID.class);
-                    when(rs.getObject(ArgumentMatchers.eq("request_id"))).thenReturn(java.util.UUID.class);
-                    when(rs.getString(ArgumentMatchers.eq("actor_id_type"))).thenReturn("string");
+
+                    when(rs.getObject(ArgumentMatchers.eq("id")))
+                            .thenReturn(UUID.fromString("9785c98c-78f2-418b-ab74-a892c3ccca9f"));
+                    when(rs.getString(ArgumentMatchers.eq("request_id")))
+                            .thenReturn("123e4567-e89b-42d3-a456-556642445678");
+                    when(rs.getString(ArgumentMatchers.eq("actor_id_type"))).thenReturn(ActorIdType.IDAM.name());
                     when(rs.getObject(ArgumentMatchers.eq("actor_id")))
                             .thenReturn("3168da13-00b3-41e3-81fa-cbc71ac28a0f");
-                    when(rs.getString(ArgumentMatchers.eq("role_type"))).thenReturn("string");
+                    when(rs.getString(ArgumentMatchers.eq("role_type"))).thenReturn(RoleType.CASE.name());
                     when(rs.getString(ArgumentMatchers.eq("role_name"))).thenReturn("Judge");
-                    when(rs.getString(ArgumentMatchers.eq("classification"))).thenReturn("PUBLIC");
-                    when(rs.getString(ArgumentMatchers.eq("grant_type"))).thenReturn("string");
-                    when(rs.getString(ArgumentMatchers.eq("role_category"))).thenReturn("string");
+                    when(rs.getString(ArgumentMatchers.eq("classification"))).thenReturn(Classification.PUBLIC.name());
+                    when(rs.getString(ArgumentMatchers.eq("grant_type"))).thenReturn(GrantType.STANDARD.name());
+                    when(rs.getString(ArgumentMatchers.eq("role_category"))).thenReturn(RoleCategory.JUDICIAL.name());
                     when(rs.getBoolean(ArgumentMatchers.eq("read_only"))).thenReturn(true);
+                    when(rs.getTimestamp(ArgumentMatchers.eq("begin_time"))).thenReturn(beginDate);
+                    when(rs.getTimestamp(ArgumentMatchers.eq("end_time"))).thenReturn(endDate);
+                    when(rs.getString(ArgumentMatchers.eq("status"))).thenReturn(Status.LIVE.toString());
+                    when(rs.getString(ArgumentMatchers.eq("reference"))).thenReturn("reference");
+                    when(rs.getString(ArgumentMatchers.eq("process"))).thenReturn("process");
+                    when(rs.getString(ArgumentMatchers.eq("attributes"))).thenReturn("attributes");
+                    when(rs.getString(ArgumentMatchers.eq("notes"))).thenReturn("notes");
+                    when(rs.getString(ArgumentMatchers.eq("log"))).thenReturn("logs");
+                    when(rs.getInt(ArgumentMatchers.eq("status_sequence"))).thenReturn(1);
+                    when(rs.getTimestamp(ArgumentMatchers.eq("created"))).thenReturn(created);
                     return resultSetExtractor.extractData(rs);
                 });
 
         List<RoleAssignmentHistory> result = sut.getLiveRecordsFromHistoryTable();
-
-        Assertions.assertEquals("PUBLIC", result.get(0).getClassification());
+        Assertions.assertEquals("IDAM", result.get(0).getActorIDType());
+        Assertions.assertEquals("CASE", result.get(0).getRoleType());
         Assertions.assertEquals("Judge", result.get(0).getRoleName());
+        Assertions.assertEquals("PUBLIC", result.get(0).getClassification());
+        Assertions.assertEquals("STANDARD", result.get(0).getGrantType());
+        Assertions.assertEquals("JUDICIAL", result.get(0).getRoleCategory());
+        Assertions.assertEquals(true, result.get(0).isReadOnly());
+        Assertions.assertEquals("LIVE", result.get(0).getStatus());
+        Assertions.assertEquals(beginDate, result.get(0).getBeginTime());
+        Assertions.assertEquals(endDate, result.get(0).getEndTime());
+        Assertions.assertEquals("reference", result.get(0).getReference());
+        Assertions.assertEquals("process", result.get(0).getProcess());
+        Assertions.assertEquals("attributes", result.get(0).getAttributes());
+        Assertions.assertEquals("notes", result.get(0).getNotes());
+        Assertions.assertEquals("logs", result.get(0).getLog());
+        Assertions.assertEquals(1, result.get(0).getStatusSequence());
+        Assertions.assertEquals(created, result.get(0).getCreated());
     }
 
     @Test
