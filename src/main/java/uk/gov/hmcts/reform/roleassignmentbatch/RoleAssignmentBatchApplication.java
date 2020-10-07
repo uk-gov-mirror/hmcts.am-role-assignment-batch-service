011e1f42c4bda1322a14aa78bba991a14adbfe6e
Nitish Singla <nitish.singla@hmcts.net>
2020-10-07 15:11:48 +0100

    Updated the time for RAS batch sleep .

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
@@ -17,8 +17,8 @@ public class RoleAssignmentBatchApplication {
     public static void main(String[] args) throws Exception {
         ApplicationContext context = SpringApplication.run(RoleAssignmentBatchApplication.class, args);
         //Sleep added to allow app-insights to flush the logs
-        //Currently, it is set to 6 hours. We need to update it later once the changes are finalized.
-        Thread.sleep(1000 * 60 * 60 * 6);
+        //Currently, it is set to 1 hours. We need to update it later once the changes are finalized.
+        Thread.sleep(1000 * 60 * 60);
         int exitCode = SpringApplication.exit(context);
         String exitCodeLog = String.format("RoleAssignmentBatchApplication Application exiting with exit code %s",
                                            exitCode);
