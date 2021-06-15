2d678cdde427ad8d9e16f154bf3704ec5fb565ad
Nitish Singla <nitish.singla@hmcts.net>
2021-06-15 12:12:19 +0100

    fix the aat.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.roleassignmentbatch;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,9 +9,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement(proxyTargetClass = true)
 @SuppressWarnings("HideUtilityClassConstructor")
+@Slf4j
 public class RoleAssignmentBatchApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(RoleAssignmentBatchApplication.class);
 
     public static void main(String[] args) throws Exception {
 
