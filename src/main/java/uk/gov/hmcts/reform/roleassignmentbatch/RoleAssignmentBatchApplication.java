d46fe70c5a0dfe2932eb38a85c53b4e84e0ee241
Nitish Singla <nitish.singla@hmcts.net>
2021-07-07 14:41:14 +0100

    removing unnecessary spring cloud jars.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.roleassignmentbatch;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 @EnableTransactionManagement(proxyTargetClass = true)
@@ -15,14 +15,14 @@ public class RoleAssignmentBatchApplication {
 
     public static void main(String[] args) throws Exception {
         String dbPass = System.getenv("ROLE_ASSIGNMENT_DB_PASSWORD");
-        if (null == dbPass || StringUtils.isEmpty(dbPass)) {
+        if (!StringUtils.hasText(dbPass)) {
             log.info("The database password is null or empty");
         } else {
             log.info("the length of dbPass is : " + dbPass.length());
         }
 
         String s2sValue = System.getenv("AM_ROLE_ASSIGNMENT_SERVICE_SECRET");
-        if (null == s2sValue || StringUtils.isEmpty(s2sValue)) {
+        if (!StringUtils.hasText(s2sValue)) {
             log.info("The s2sValue is null or empty");
         } else {
             log.info("the length of s2sValue is : " + s2sValue.length());
