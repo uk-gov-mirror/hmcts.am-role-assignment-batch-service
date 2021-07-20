47da92e48d2b36a99147ad24902e791e27434d7f
Nitish Singla <nitish.singla@hmcts.net>
2021-07-20 10:16:34 +0100

    Additional logging.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/ApplicationParams.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/ApplicationParams.java
@@ -6,24 +6,19 @@ import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.ENABLED;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
 
 @Named
 @Singleton
+@ToString
 public class ApplicationParams {
-
-    @Value("${audit.exception.enabled:true}")
-    private boolean auditEnabled;
 
     @Value("${migration.masterFlag:false}")
     private boolean processCcdDataAllowed;
 
     @Value("${migration.renameTables:false}")
     private boolean renamingPostMigrationTablesEnabled;
-
-    public boolean isAuditEnabled() {
-        return auditEnabled;
-    }
 
     public String getProcessCcdDataEnabled() {
         return processCcdDataAllowed ? ENABLED : DISABLED;
--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/config/JobRunnableDecider.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/config/JobRunnableDecider.java
@@ -1,14 +1,16 @@
 package uk.gov.hmcts.reform.roleassignmentbatch.config;
 
+import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.DISABLED;
+import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.ENABLED;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.reform.roleassignmentbatch.ApplicationParams;
 import uk.gov.hmcts.reform.roleassignmentbatch.launchdarkly.FeatureConditionEvaluator;
-import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.ENABLED;
-import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.DISABLED;
 
 @Slf4j
 public class JobRunnableDecider implements JobExecutionDecider {
@@ -18,10 +20,15 @@ public class JobRunnableDecider implements JobExecutionDecider {
     @Autowired
     FeatureConditionEvaluator featureConditionEvaluator;
 
+    @Autowired
+    ApplicationParams applicationParams;
+
     @Override
     public FlowExecutionStatus decide(JobExecution job, StepExecution stepExecution) {
         boolean isEnabled = featureConditionEvaluator.isFlagEnabled(SERVICE_NAME, job.getJobInstance().getJobName());
         log.info("***** LdFlag for {} is enabled {} *****", job.getJobInstance().getJobName(), isEnabled);
+        log.info("***** The Application params for Job {} run are {} *****", job.getJobInstance().getJobName(),
+                 applicationParams.toString());
         return new FlowExecutionStatus(isEnabled ? ENABLED : DISABLED);
     }
 }
