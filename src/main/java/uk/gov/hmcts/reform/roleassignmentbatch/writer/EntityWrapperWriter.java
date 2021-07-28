52cf3fd7cbb67b9a608092d3db380581405e39b3
Nitish Singla <nitish.singla@hmcts.net>
2021-07-28 12:25:32 +0100

    AM-1542: Fix the performance issue where 10,000 records are taking 30 seconds.

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/writer/EntityWrapperWriter.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/writer/EntityWrapperWriter.java
@@ -1,8 +1,9 @@
 package uk.gov.hmcts.reform.roleassignmentbatch.writer;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.reform.roleassignmentbatch.entities.HistoryEntity;
 import uk.gov.hmcts.reform.roleassignmentbatch.entities.RequestEntity;
 import uk.gov.hmcts.reform.roleassignmentbatch.entities.RoleAssignmentEntity;
 
+@Slf4j
 public class EntityWrapperWriter implements ItemWriter<EntityWrapper> {
 
     @Autowired
@@ -24,11 +26,24 @@ public class EntityWrapperWriter implements ItemWriter<EntityWrapper> {
 
     @Override
     public void write(List<? extends EntityWrapper> items) throws Exception {
-        for (EntityWrapper item : items) {
-            requestEntityWriter.write(Collections.singletonList(item.getRequestEntity()));
-            roleAssignmentHistoryWriter.write(Collections.singletonList(item.getRoleAssignmentHistoryEntity()));
-            roleAssignmentWriter.write(Collections.singletonList(item.getRoleAssignmentEntity()));
-            // Actor cache entity will be built in a separate step.
-        }
+        final long currentTime = System.currentTimeMillis();
+
+        List<RequestEntity> requestEntities = items.stream()
+                                                   .map(EntityWrapper::getRequestEntity)
+                                                   .collect(Collectors.toList());
+        requestEntityWriter.write(requestEntities);
+
+        List<HistoryEntity> historyEntities = items.stream()
+                                                   .map(EntityWrapper::getRoleAssignmentHistoryEntity)
+                                                   .collect(Collectors.toList());
+        roleAssignmentHistoryWriter.write(historyEntities);
+
+        List<RoleAssignmentEntity> roleAssignmentEntities = items.stream()
+                                                                 .map(EntityWrapper::getRoleAssignmentEntity)
+                                                                 .collect(Collectors.toList());
+        roleAssignmentWriter.write(roleAssignmentEntities);
+
+        log.info("Time taken for :" + items.size() + " items is : "
+                           + (System.currentTimeMillis() - currentTime));
     }
 }
