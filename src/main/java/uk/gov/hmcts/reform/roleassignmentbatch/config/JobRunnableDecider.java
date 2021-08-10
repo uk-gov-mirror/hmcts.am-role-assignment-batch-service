983e9c9dbcc177c52de88be9074443c99873da93
ConorH994 <conor.halpin@hmcts.net>
2021-08-10 16:36:43 +0100

    adding email notification when an error with flags occurs

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/config/JobRunnableDecider.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/config/JobRunnableDecider.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.roleassignmentbatch.config;
 
 import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.DISABLED;
 import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.ENABLED;
+import static uk.gov.hmcts.reform.roleassignmentbatch.util.Constants.PROCESS_FLAGS;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
@@ -11,6 +12,7 @@ import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.reform.roleassignmentbatch.ApplicationParams;
+import uk.gov.hmcts.reform.roleassignmentbatch.domain.model.EmailData;
 import uk.gov.hmcts.reform.roleassignmentbatch.launchdarkly.FeatureConditionEvaluator;
 import uk.gov.hmcts.reform.roleassignmentbatch.service.EmailService;
 
@@ -54,18 +56,22 @@ public class JobRunnableDecider implements JobExecutionDecider {
 
     public boolean processExecutionDecider(JobExecution job) {
         var isEnabled = true;
-        var ldMigrationFlag = featureConditionEvaluator.isFlagEnabled(SERVICE_NAME,
-                "ccd-am-migration");
-        var ldRenameTablesFlag = featureConditionEvaluator.isFlagEnabled(SERVICE_NAME,
-                "ccd-am-migration-rename-tables");
+
+        var ldMigrationFlag = getFluxFlagStatus("ccd-am-migration");
+        var ldRenameTablesFlag = getFluxFlagStatus("ccd-am-migration-rename-tables");
+
+        var flagStatus = buildFlagStatusString(ldMigrationFlag, ldRenameTablesFlag);
+        var emailData = buildEmailData(flagStatus, job.getJobId().toString());
 
         if (fluxMigrationFlag != ldMigrationFlag || fluxRenameTablesFlag != ldRenameTablesFlag) {
             log.info("LaunchDarkly and Flux flags do not match");
+            emailService.sendEmail(emailData);
             job.setExitStatus(new ExitStatus(ExitStatus.STOPPED.toString(),
                     "LaunchDarkly and Flux flags do not match"));
             isEnabled = false;
         } else if (fluxMigrationFlag && fluxRenameTablesFlag) {
             log.info("migrationFlag and renameTablesFlag are true");
+            emailService.sendEmail(emailData);
             job.setExitStatus(new ExitStatus(ExitStatus.STOPPED.toString(),
                     "migrationFlag and renameTablesFlag are true"));
             isEnabled = false;
@@ -75,5 +81,27 @@ public class JobRunnableDecider implements JobExecutionDecider {
         }
 
         return isEnabled;
+    }
+
+    public String buildFlagStatusString(boolean ldmf, boolean ldrf) {
+        return String.format("LD Migration Flag: %s <br />"
+                        + "LD Rename Flag: %s <br />"
+                        + "Flux Migration Flag: %s <br />"
+                        + "Flux Rename Flag: %s <br />",
+                ldmf, ldrf, fluxMigrationFlag, fluxRenameTablesFlag);
+    }
+
+    public EmailData buildEmailData(String flagStatus, String jobId) {
+        return EmailData
+                .builder()
+                .runId(jobId)
+                .emailSubject(PROCESS_FLAGS)
+                .module("ProcessFlags")
+                .flags(flagStatus)
+                .build();
+    }
+
+    public boolean getFluxFlagStatus(String flagName) {
+        return featureConditionEvaluator.isFlagEnabled(SERVICE_NAME, flagName);
     }
 }
--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/util/Constants.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/util/Constants.java
@@ -105,6 +105,7 @@ public class Constants {
     public static final String AFTER_VALIDATION = "Validation - Reconciliation Status";
 
     public static final String RECONCILIATION = "Reconciliation";
+    public static final String PROCESS_FLAGS = "ProcessFlags";
     public static final String DELETE_EXPIRED_JOB = "DeleteExpiredJob";
     public static final String DELETE_EXPIRED_RECORD_JOB_STATUS = "Delete Expired Record Job Status";
     public static final String ZERO_COUNT_IN_CCD_VIEW = "ZERO";
