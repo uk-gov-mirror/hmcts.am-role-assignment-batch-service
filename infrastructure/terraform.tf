45b831d593fe4df12d2eaa662d2158833d841b3c
arshinsalim <arshin.salim@hmcts.net>
2022-08-01 11:58:18 +0100

    combine renovate PR's

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.9.0"
+      version = "~> 3.16.0"
     }
     random = {
       source = "hashicorp/random"
