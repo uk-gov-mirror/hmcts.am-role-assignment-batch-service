9885a8f718e2989e81f6bfe202e7857be7103773
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2023-04-14 14:08:01 +0100

    AM-2750: Introduce telemetry flush to main method

--- src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
+++ src/main/java/uk/gov/hmcts/reform/roleassignmentbatch/RoleAssignmentBatchApplication.java
@@ -1,6 +1,8 @@
 package uk.gov.hmcts.reform.roleassignmentbatch;
 
+import com.microsoft.applicationinsights.TelemetryClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,15 +14,30 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Slf4j
 public class RoleAssignmentBatchApplication {
 
-    public static void main(String[] args) throws Exception {
+    @Autowired
+    private static TelemetryClient client;
+
+    @Autowired
+    public RoleAssignmentBatchApplication(TelemetryClient client) {
+        RoleAssignmentBatchApplication.client = client;
+    }
+
+    public static void main(String[] args) {
         final ApplicationContext context = SpringApplication.run(RoleAssignmentBatchApplication.class, args);
-        log.info("Putting application to sleep");
-        Thread.sleep(1000 * 5L);
-        log.info("The sleep is complete.");
-        int exitCode = SpringApplication.exit(context);
-        String exitCodeLog = String.format("RoleAssignmentBatchApplication Application exiting with exit code %s",
-                                           exitCode);
-        log.info(exitCodeLog);
-        System.exit(exitCode);
+
+        try {
+            log.info("Putting application to sleep");
+            Thread.sleep(1000 * 5L);
+            log.info("The sleep is complete.");
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        } finally {
+            client.flush();
+            int exitCode = SpringApplication.exit(context);
+            String exitCodeLog = String.format("RoleAssignmentBatchApplication Application exiting with exit code %s",
+                    exitCode);
+            log.info(exitCodeLog);
+            System.exit(exitCode);
+        }
     }
 }
